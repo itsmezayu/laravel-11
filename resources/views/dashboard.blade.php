f1287c8bcdb8b68eb11c1f3bf35744273b7dbb82
itsmezayu <shahzada.sabih@gmail.com>
2025-08-21 11:39:43 +0700

    #

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,12 @@
 <x-app-layout>
+    {{-- Header --}}
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
+    {{-- Main Content --}}
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             {{-- KOTAK SAPAAN PENGGUNA --}}
